b337e6da0a2eafcb499252efdc6e297f3a053e13
marimari118 <marimari118yt@gmail.com>
2023-12-17 19:25:53 +0900

    フッターをページ最下部に固定されるよう変更

--- resources/views/task/includes/head.blade.php
+++ resources/views/task/includes/head.blade.php
@@ -1,4 +1,5 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
 <title>{{ $title }}</title>
